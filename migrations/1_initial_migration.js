681affadcfc4a0a885a812b07a9119b0de61e59b
Alvin Wo <haosuwo@gmail.com>
2023-04-06 00:08:11 +0800

    write ut for factory and token contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,14 @@
-const { accounts } = require("@openzeppelin/test-environment");
+require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider, environment: 'truffle' });
+const { singletons } = require("@openzeppelin/test-helpers");
 
 const ProjectX = artifacts.require("ProjectX");
 const ProjectXFactory = artifacts.require("ProjectXFactory");
-module.exports = function (deployer) {
-    deployer.deploy(ProjectXFactory);
-    deployer.deploy(ProjectX, accounts[0], true);
+const ProjectXToken = artifacts.require("ProjectXToken");
+module.exports = async function (deployer, network, accounts) {
+    if(network === 'development') {
+        await singletons.ERC1820Registry(accounts[0]);
+    }
+    await deployer.deploy(ProjectXToken, 1000, [accounts[0], accounts[1]]);
+    await deployer.deploy(ProjectXFactory);
+    await deployer.deploy(ProjectX, accounts[0], true);
 };
